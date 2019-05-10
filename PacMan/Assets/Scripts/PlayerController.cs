e8155fc71096f31dbda44d9ed89cb88e64f673dd
Aimé Yamamoto <aimeyamamoto@gmail.com>
2019-05-10 13:34:41 -0400

    Player Basic Movement
    pacman controls and movement

---
+++ PacMan/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private PlayerMotor _playerMotor;
+
+    public Vector3 Up = new Vector3(0f, 0f, 1f);
+    public Vector3 Down = new Vector3(0f, 0f, -1f);
+    public Vector3 Right = new Vector3(1f, 0f, 0f);
+    public Vector3 Left = new Vector3(-1f, 0f, 0f);
+    void Awake()
+    {
+        _playerMotor = GetComponent<PlayerMotor>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Up") && _playerMotor.CheckDirection(Up))
+        {
+            _playerMotor.Up();
+        }
+        if (Input.GetButtonDown("Down") && _playerMotor.CheckDirection(Down))
+        {
+            _playerMotor.Down();
+        }
+        if (Input.GetButtonDown("Right") && _playerMotor.CheckDirection(Right))
+        {
+            _playerMotor.Right();
+        }
+        if (Input.GetButtonDown("Left") && _playerMotor.CheckDirection(Left))
+        {
+            _playerMotor.Left();
+        }
+    }
+}
---
+++ PacMan/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody _rigidbody;
+    private Transform _transform;
+    public bool validDirection;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _transform = GetComponent<Transform>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        NonStopMove();
+    }
+
+    public void NonStopMove()
+    {
+        if (CheckDirection(_transform.forward))
+            _rigidbody.MovePosition(_transform.position + speed * Time.deltaTime * _transform.forward);
+    }
+
+    public void Up()
+    {
+        Rotate(0f);
+    }
+
+    public void Down()
+    {
+        Rotate(180f);
+    }
+
+    public void Right()
+    {
+        Rotate(90f);
+    }
+
+    public void Left()
+    {
+        Rotate(-90f);
+    }
+
+    public void Rotate(float yAngle)
+    {
+        _transform.rotation = Quaternion.Euler(0f, yAngle, 0f);
+    }
+
+    public bool CheckDirection(Vector3 direction)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(_transform.position, direction, out hit, 0.55f))
+        {
+            return (!hit.transform.CompareTag("Wall"));
+        }
+        return true;
+    }
+}
