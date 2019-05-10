b693caa6f21ce4289ebdedc9fd0cadbc55728abd
Aimé Yamamoto <aimeyamamoto@gmail.com>
2019-05-10 16:34:14 -0400

    Basic UI
    Scoring displayed

---
+++ PacMan/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public PlayerController _player;
+    private const int totalDots = 312;
+
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_player.dotsEaten == totalDots)
+        {
+            // GG WP
+        }
+        else
+        {
+            
+        }
+    }
+}
---
+++ PacMan/Assets/Scripts/UIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text scoreValue;
+    private GameController _gameController;
+
+    private void Awake()
+    {
+        _gameController = GetComponent<GameController>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreValue.text = _gameController._player.dotsEaten.ToString();
+    }
+}
