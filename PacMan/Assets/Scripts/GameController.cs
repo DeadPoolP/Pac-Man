217708cac6a1735e612522603e04958f51dd3fc9
Aimé Yamamoto <aimeyamamoto@gmail.com>
2019-05-12 23:19:47 -0400

    UI GameOver + Start Again Input
    Updated Panel in UI 
    Press Space to restart level

--- PacMan/Assets/Scripts/GameController.cs
+++ PacMan/Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -10,21 +11,50 @@ public class GameController : MonoBehaviour
     public Dot exampleDot;
     public BigDot exampleBigDot;
     public int killValue = 10;
-
+    public bool gameover = false;
     // Start is called before the first frame update
     void Start()
     {
+        gameover = false;
         ui = GetComponent<UIController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ui.UpdateLives(_player.health);
-        ui.UpdateScore(_player.dotsEaten * exampleDot.getScoreValue() + _player.bigDotsEaten * exampleBigDot.getScoreValue() + _player.killCount * killValue);
-        if (_player.dotsEaten == totalDots || _player.health <= 0)
+        if (!gameover)
         {
-            ui.DisplayGameOverPanel();
+            ui.UpdateLives(_player.health);
+            ui.UpdateScore(_player.dotsEaten * exampleDot.getScoreValue() + _player.bigDotsEaten * exampleBigDot.getScoreValue() + _player.killCount * killValue);
+            if (_player.dotsEaten == totalDots || _player.health <= 0)
+            {
+                EndGame();
+            }
         }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+                StartGame();
+            }
+        }
+        
+        
+    }
+
+    private void StartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    private void EndGame()
+    {
+        if (_player.health <= 0)
+            Destroy(_player.gameObject);
+        ui.DisplayGameOverPanel();
+        Time.timeScale = 0f;
+        gameover = true;
     }
 }
